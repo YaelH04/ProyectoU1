ebe74b82f85d242f876822271237031f13663816
YaelH <yael.hermosillo07@gmail.com>
2025-09-16 01:02:47 -0600

    se elimino el archivo conexion.py

--- conexion.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = "super-clave-secreta"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:micontraseña@localhost/miniproyecto"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
